850e93f08e89a02219f24991f3ca4cb8c22e81e1
Andrei Nistor <andrei@nistor.tech>
2022-09-29 03:04:25 +0300

    Fix typo in transformed_stage_url (closes #3)

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
   snowflake_loader_user        = var.override_snowflake_loader_user != "" ? var.override_snowflake_loader_user : "${upper(var.name)}_LOADER_USER"
   snowflake_loader_role        = var.override_snowflake_loader_role != "" ? var.override_snowflake_loader_role : "${upper(var.name)}_LOADER_ROLE"
   transformed_stage_name       = "${upper(var.name)}_TRANSFORMED_STAGE"
-  transformed_stage_url        = var.override_transformed_stage_url != "" ? var.override_transformed_stage_url : "s3://${var.stage_bucket}/tranformed/good"
+  transformed_stage_url        = var.override_transformed_stage_url != "" ? var.override_transformed_stage_url : "s3://${var.stage_bucket}/transformed/good"
   folder_monitoring_stage_name = "${upper(var.name)}_FOLDER_MONITORING_STAGE"
   folder_monitoring_stage_url  = var.override_folder_monitoring_stage_url != "" ? var.override_folder_monitoring_stage_url : "s3://${var.stage_bucket}/${var.name}/monitoring"
   snowflake_iam_load_role_name = var.override_iam_loader_role_name != "" ? var.override_iam_loader_role_name : "${var.name}-snowflakedb-load-role"
